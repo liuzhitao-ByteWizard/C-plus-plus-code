9cda8ef8ed816698e6c619ce359565065c82cdc7
byte-wizard <liuzhitao2026@163.com>
2026-09-20 23:05:47 +0800

    1

---
+++ 2026_9_20/2026_9_20/main.cpp
@@ -0,0 +1,24 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <string>
+using namespace std;
+
+void func() {
+	string s1("hello world hello world hello world hello world hello world hello world"); // 把空格换成%%
+	string s2;
+	s2.reserve(100);
+	for (auto& ch : s1) {
+		if (ch == ' ') {
+			s2 += "%%";
+		}
+		else if (ch != ' ') {
+			s2 += ch;
+		}
+	}
+	cout << s2 << endl;
+}
+
+int main() {
+	func();
+	return 0;
+}
